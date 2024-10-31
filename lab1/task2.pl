52916badcfb9d226ed590bc94066e42d299015a1
luckyabatur <dekhterenkodmitrii@gmail.com>
2024-10-31 21:25:05 +0300

    task2 other variant

--- lab1/task2.pl
+++ lab1/task2.pl
@@ -6,17 +6,9 @@ groups(L) :- setof(G, A^B^C^grade(G, A, B, C), L).
 sum([], 0).
 sum([X|L], T) :- sum(L, T1), T is X + T1.
 
-average(S, A) :- findall(X, grade(_, _, S, X), L), length(L, N), sum(L, T), A is T / N.
-average_all(S, A) :- subjects(L), member(S, L),  average(S, A).
+average(S, A) :- findall(X, grade(S, _, _, X), L), length(L, N), sum(L, T), A is T / N.
+average_all(S, A) :- groups(L), member(S, L),  average(S, A).
+
+faillist(S, L) :- subjects(T), member(S, T), findall(S, grade(_, _, S, 2), L). 
 
 failg(G, C) :- groups(T), member(G, T), findall(G, grade(G, _, _, 2), L), length(L, C).
-
-fails(S, C) :- subjects(T), member(S, T), findall(S, grade(_, _, S, 2), L), length(L, C).
-
-
-main :-
-    average_all(S, A), write(S), write(' '), write(A), nl, fail.
-main :-
-    failg(G, C), write(G), write(' '), write(C), nl, fail.    
-main :- 
-    fails(S, C), write(S), write(' '), write(C), nl, fail.
